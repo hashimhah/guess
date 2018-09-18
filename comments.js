cc9d6c11e3938576ca223607d501cc51a703ddc6
hashimhah <42972727+hashimhah@users.noreply.github.com>
2018-09-18 09:16:01 -0400

    just the comments

---
+++ comments.js
@@ -0,0 +1,72 @@
+// *** Guessing Game by Bram Moreinis, Burlington Tech Center Teacher ***
+
+// *** Initializations ***
+
+// ** Basic Initializations **
+
+// var answer integer, set to 100
+
+// var guess integer, set to 0
+
+// var turns integer, set to 1
+
+// var correctAnswer boolean, set to false
+
+// ** Advanced Initializations **
+
+// var averageTurns integer, set to 0
+
+// var totalTurns integer, set to 0
+
+// var games integer, set to 1
+
+// var again string, set to "Y"
+
+// var playAgain boolean, set to true
+
+
+// *** Game Play ***
+
+// ** Advanced loop start **
+// keep looping while playAgain = true
+// !!! Inside conditions use two equals ==
+
+// ** Basic loop start **
+// answer = random number between 0 and 100
+
+// print answer to console log while testing
+
+// reset correctAnswer to false
+
+// Keep looping while correctAnswer is false
+
+    
+   // prompt user for guess, tell user what to input
+
+   // check if guess is equal to answer
+
+       // if equal, say "Correct!" and say number of turns
+
+       // set correctAnswer as true to get out of loop
+
+   // check if guess greater than answer
+       // give feedback: go lower
+   // check if guess lower than answer
+       // give feedback: go higher
+
+   // add one to turns
+
+// ** Basic loop end
+
+// add 1 to games
+// add turns to totalTurns
+// set averageTurns = totalTurns / games
+// say "Your average is " averageTurns  "over " games "games."
+
+// ask to play again (y = yes).
+// if not y, set playAgain = false'
+
+// ** Game loop end
+
+// say thank for playing.
+// *** Game end
